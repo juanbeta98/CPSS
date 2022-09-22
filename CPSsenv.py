02d95b5adde4c6623f4b0b799e4cc2561926dee6
juanbeta98 <juan.beta98@gmail.com>
2022-09-21 19:35:58 -0500

    First update on environment

--- CPSsenv.py
+++ CPSsenv.py
@@ -1,10 +1,11 @@
 '''
-Author: Juan Betancourt 
+Authors: 
+- Juan Betancourt
+- Germán Pardo
 '''
 
 import networkx as nx
 from numpy.random import random
-
 
 class CPSsenv():
 
@@ -18,16 +19,13 @@ class CPSsenv():
         self.Goals = [node for node in self.nw.nodes() if self.nw.nodes()[node]['type']== 'Goal']
         self.Skills = [node for node in self.nw.nodes() if self.nw.nodes()[node]['type']== 'Skill']
 
-        self.max_steps = 30
+        self.max_steps = 20
 
 
-
-    def reset(self, l, random_start = False):
+    def reset(self, g_state = False):
         self.t = 0
 
-        
-            
-        
+        # Reseting the environment to the network configuration
         self.acc = {node:0 if self.nw.nodes()[node]['D'] == False else 1 for node in self.Accesses}
         self.kno = {node:0 if self.nw.nodes()[node]['D'] == False else 1 for node in self.Knowledges}
         self.goa = {node:0 if self.nw.nodes()[node]['D'] == False else 1 for node in self.Goals}
@@ -36,57 +34,28 @@ class CPSsenv():
 
         self.collection = [node for node in self.nw.nodes() if self.nw.nodes()[node]['type'] in ['Access', 'Knowledge','Skill' , 'Goal'] and self.nw.nodes()[node]['D'] == True]
 
-        
-                
-        ### l recibe un estado y lo actualiza en el ambiente
-        if l:
-            
+        # Reseting the environment to a given state
+        if g_state:
+            self.collection = []
             for node in self.Accesses:
-                    self.acc[node] = l[0][node]
-                    
-                    if l[0][node] == 1:
-                        self.collection.append(node)
-                     #   self.nw.nodes()[node]['D'] = True
-                        
+                self.acc[node] = g_state[0][node] 
+                if g_state[0][node] == 1:
+                    self.collection.append(node) 
                         
             for node in self.Knowledges:
-                    self.kno[node] = l[1][node]
-                    
-                    if l[1][node] == 1:
-                        self.collection.append(node)
-                    #    self.nw.nodes()[node]['D'] = True
-                    
-                    
+                self.kno[node] = g_state[1][node]
+                if g_state[1][node] == 1:
+                    self.collection.append(node)
                    
             for node in self.Skills:
-                    self.ski[node] = l[2][node]
-                    
-                    if l[2][node] == 1:
-                        self.collection.append(node)
-                    #    self.nw.nodes()[node]['D'] = True
-                        
+                self.ski[node] = g_state[2][node]
+                if g_state[2][node] == 1:
+                    self.collection.append(node)  
             
             for node in self.Goals:
-                    self.goa[node] = l[3][node]
-                    
-                    if l[3][node] == 1:
-                        self.collection.append(node)
-                    #    self.nw.nodes()[node]['D'] = True
-        
-        
-        if random_start: 
-            for access in self.Accesses:
-                if random() < 0.5:
-                    self.acc[access] = 1
-                    self.collection.append(access)
-            for knowledge in self.Knowledges:
-                if random() < 0.5:
-                    self.kno[knowledge] = 1
-                    self.collection.append(knowledge)
-            for skill in self.Skills:
-                if random() < 0.5:
-                    self.ski[skill] = 1
-                    self.collection.append(skill)
+                self.goa[node] = g_state[3][node]
+                if g_state[3][node] == 1:
+                    self.collection.append(node)
 
         self.available_actions = self.get_available_actions()
         self.state = self.assemble_state()
@@ -94,20 +63,21 @@ class CPSsenv():
         return self.state, self.available_actions
 
     
-    
-    ### Función step para Q-learning
-    
-    def step(self, action):
-        W = self.gen_W(action)
+    def step(self, action, stochastic = True):
+        if stochastic:
+            W = self.gen_W(action)
+        else:
+            W = True
+
         cost = self.nw.nodes()[action]['C']
         _ = ''
 
-        payoff = self.update_state(action, W)
+        payoff, done = self.update_state(action, W)
         
         reward = payoff - cost
         self.state = self.assemble_state()
 
-        done = self.check_termination()
+        done = self.check_termination(done)
 
         self.available_actions = self.get_available_actions()
         self.t += 1
@@ -115,52 +85,21 @@ class CPSsenv():
         return self.state, self.available_actions, reward, done, _
     
     
-    
-    ### Función step para vaule iteration
-    
-    def step2(self, action):
-        W = True
-        cost = self.nw.nodes()[action]['C']
-        _ = ''
-
-        payoff = self.update_state(action, W)
-        
-        reward = payoff - cost
-        self.state = self.assemble_state()
-
-        done = self.check_termination()
-
-        self.available_actions = self.get_available_actions()
-        self.t += 1
-        
-        return self.state, self.available_actions, reward, done, _
-    
-    
-    
-    
-
     def assemble_state(self):
         state = [self.acc, self.kno, self.ski, self.goa]
         return state
 
-    
-    ### Función gen_W para Q-learning
-    
+
     def gen_W(self, action):
         W = False
         if self.nw.nodes()[action]['p'] > random():
             W = True
         return W
     
-    ### Función gen_W para value iteration
-    def gen_W2(self, action):
-        W = False
-        if self.nw.nodes()[action]['p'] > 0: 
-            W = True
-        return W
     
     def update_state(self, action, W):
         payoff = 0
+        done = False
         if W:
             for edge in self.nw.edges(action):
                 node = edge[1]
@@ -174,15 +113,14 @@ class CPSsenv():
                         payoff += 1
                     
                         self.goa[node] = 1
-                        
+
                     self.collection.append(node)
+
             self.act[action] = 1       
         
-        return payoff
+        return payoff, done
     
-    def check_termination(self):
-        done = 0 not in self.goa.values()
-    
+    def check_termination(self, done):    
         # Number of time-steps
         if self.t > self.max_steps:
             done = True
